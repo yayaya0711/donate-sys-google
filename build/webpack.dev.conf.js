4129c755291ec404a0a63381a74e38dfd6913d51
yayaya0711 <38532955+yayaya0711@users.noreply.github.com>
2020-06-10 20:53:08 +0800

    test

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -58,13 +58,22 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       inject: true
     }),
     // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.dev.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: path.resolve(__dirname, '../static'),
+    //     to: config.dev.assetsSubDirectory,
+    //     ignore: ['.*']
+    //   }
+    // ])
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          // from: path.join(__dirname,'assets'),
+          // to: 'assets'
+          from: path.resolve(__dirname, '../static')
+        }
+      ],
+    })
   ]
 })
 
